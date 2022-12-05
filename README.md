b5cba78c8e2f614b86aabd0f60e739ac494ac58f
Markus Meier <markus.meier@gbtec.de>
2022-12-05 14:59:56 +0100

    Setup project

---
+++ README.md
@@ -0,0 +1,8 @@
+# py-click Demo
+
+## Usage
+
+```bash
+python3 -m venv .venv
+source ./.venv/bin/activate
+```
--- verify.sh
+++ verify.sh
@@ -1,32 +1,24 @@
 #!/usr/bin/env bash
 
-# FIXME: replace the following with poetry run
-# https://python-poetry.org/docs/basic-usage/
-
 cyan=$(tput setaf 37)    # info
 reset=$(tput sgr0)
 
-echo "${cyan}Installing (dev) dependencies...${reset}"
-pip install --requirement requirements.txt
+echo "${cyan}Installing dependencies...${reset}"
+poetry install
 
 echo "${cyan}Sorting imports...${reset}"
-fdfind --exclude env \
-       --exclude '.env' \
-       --exclude venv \
-       --exclude '.venv' \
-       py \
-      | xargs isort
+fdfind --exclude '.venv' python3 | xargs isort
 
 echo "${cyan}Removing unused imports...${reset}"
 autoflake --in-place \
           --remove-all-unused-imports \
           --recursive \
-          --exclude '.git,.idea,.pytest_cache,.vscode,env,.env,venv,.venv' .
+          --exclude '.git,dist,.venv' .
 
 echo "${cyan}Formatting the code...${reset}"
 black --line-length 130 \
-      --exclude '/(.git|\.idea|.pytest_cache|.vscode|__pycache__|env|.env|venv|.venv)/' \
+      --exclude '/(.git|dist|.venv)/' \
       .
 
 echo "${cyan}Executing tests...${reset}"
-python3 -m unittest
+pytest
